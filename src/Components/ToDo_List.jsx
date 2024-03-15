eb9164e524ae3bf8596a1d5923151ee5f47dbf69
Your Usernameclear <151780860+gee4star@users.noreply.github.com>
2024-03-14 20:02:36 -0700

    First ToDo_List

---
+++ src/Components/ToDo_List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ToDo_List() {
+    return(<div>
+        <div>
+            <input type="checkbox"/>
+            <input type="text" />
+            <button>edit</button>
+            <button>delete</button>
+        </div>
+    </div>)
+}
+
+export default ToDo_List
