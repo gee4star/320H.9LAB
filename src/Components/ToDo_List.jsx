30b00cca577a6b536187b5d4ca4951f4558b3613
Your Usernameclear <151780860+gee4star@users.noreply.github.com>
2024-03-15 22:27:27 -0700

    Changes to ToDo_List

--- src/Components/ToDo_List.jsx
+++ src/Components/ToDo_List.jsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const initialListItems = [
+     "Do this first"
+]
+
 
 function ToDo_List() {
-    return(<div>
+    
+    const [todos, createTodos] = useState(initialListItems);
+    // const addItem = obj => {
+    //     setTodos(current => [...current, obj]);
+    //   };
+    // const input = document.getElementById("input")  
+
+    //   function addItems() {
+        //                 setTodos([
+        //       ...todos,
+        //       { id: ids++, name: name }
+        //     ]);
+        //   }
+    return(
+    <div>
         <div>
+            <input
+                type="text"
+                aria-label="Field name"
+                id="input"
+                // onChange={}
+                // value={value}
+                // onChange={onChange}
+            />
+              {/* const handleAdd = (todo) => {
+                const newTodos = todos.slice();
+                newTodos.push(todo);
+                setTodos(newTodos);
+                } */}
+            <button 
+                onClick={() => 
+                    createTodos((array) => array.concat(document.getElementById("input").value))}
+                //     addItem({
+                //       id: Math.random(),
+                //       name: input
+                //     })
+                //   }
+            >create</button>
+            {todos.map((a, i) => (
+          <p key={i}>{a}</p>
+        ))}
+           
+
+
+
             <input type="checkbox"/>
-            <input type="text" />
             <button>edit</button>
             <button>delete</button>
         </div>
-    </div>)
+    </div>
+    )
 }
 
 export default ToDo_List
